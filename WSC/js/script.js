337bf8fe2af57d29edead86ff8b817303d148c1b
Sergiy888 <loki_888@i.ua>
2016-02-02 00:01:40 +0200

    В нижній частині сайту додано альтернативну галерею

--- WSC/js/script.js
+++ WSC/js/script.js
@@ -25,4 +25,66 @@ $(document).ready(function(){
 	});
 });
 
+$(document).ready(function() {
+ var owl = $("#owl-demo");
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+	  autoPlay:true,
+	  navigation :false,
+	  paginationNumbers: false,
+	  pagination:false
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+	  
+	  
+	  
+	  
+  });
+ 
+	
+	
+// Custom Navigation Events
+  $(".next").click(function(){
+    owl.trigger('owl.next');
+  })
+  $(".prev").click(function(){
+    owl.trigger('owl.prev');
+  })
 
+
+   $('.first').click(function(){
+    owl.trigger('owl.goTo', 0)
+  }); 
+	  $(".second").click(function(){
+    owl.trigger('owl.goTo', 1)
+  }); 
+    $(".third").click(function(){
+    owl.trigger('owl.goTo', 2)
+  }); 
+    $(".forth").click(function(){
+    owl.trigger('owl.goTo', 3)
+  }); 
+    $(".fives").click(function(){
+    owl.trigger('owl.goTo', 4)
+  }); 
+  
+  
+  
+ 
+  
+  
+  
+  
+  
+  
+
+});
